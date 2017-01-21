54299879d54648903fe8d84f6439ebb85762bb62
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2017-01-21 16:03:38 +0530

    A UIDatePicker object is a control used for the inputting of date and time values. You can use a date picker to allow a user to enter either a point in time (calendar date, time value or both) or a time interval (for example for a timer). The date picker reports interactions to its associated target object.

--- UIDatePicker/ViewController.swift
+++ UIDatePicker/ViewController.swift
@@ -13,6 +13,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create a DatePicker
+        let datePickerView: UIDatePicker = UIDatePicker()
+        
+        // Posiiton date picket within a view
+        datePickerView.frame = CGRect(x: 10, y: 50, width: self.view.frame.width, height: 200)
+        
+        // Set some of UIDatePicker properties
+        datePickerView.timeZone = NSTimeZone.local
+        datePickerView.backgroundColor = UIColor.white
+        
+        // Add an event to call onDidChangeDate function when value is changed.
+        datePickerView.addTarget(self, action: #selector(ViewController.onDidChangeDate(_:)), for: .valueChanged)
+        
+        // Add DataPicker to the view
+        self.view.addSubview(datePickerView)
+    }
+    
+    
+    func onDidChangeDate(_ sender: UIDatePicker){
+        
+        // Create date formatter
+        let dateFormatter: DateFormatter = DateFormatter()
+        
+        // Set date format
+        dateFormatter.dateFormat = "MM/dd/yyyy hh:mm a"
+        
+        // Apply date format
+        let selectedDate: String = dateFormatter.string(from: sender.date)
+        
+        print("Selected date  : \(selectedDate)")
     }
 
     override func didReceiveMemoryWarning() {
